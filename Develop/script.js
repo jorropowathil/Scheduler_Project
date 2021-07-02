35cc54c22df95ed8ee6762c4fcc2fbe73e356b0f
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-07-01 22:04:16 -0400

    initial commit, linked js & css and started assigning variables

---
+++ Develop/script.js
@@ -0,0 +1,8 @@
+var containerEl = document.getElementsByClassName("container")
+var todayDateEl = moment();
+$("#currentDay").text(today.format("LL HH:mm"));
+
+
+function
+// Create hour time blocks for each hour of the day using for loops
+// Create click events 
